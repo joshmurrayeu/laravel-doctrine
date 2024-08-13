066fa0491e61bc7867e5a3a677634fbab748f12a
Josh Murray <josh.murray@nmible.com>
2024-08-13 10:59:01 +0100

    Updated the Repository to allow filters and sorts

--- src/Repositories/Abstracts/Repository.php
+++ src/Repositories/Abstracts/Repository.php
@@ -5,26 +5,112 @@ declare(strict_types=1);
 namespace LaravelDoctrine\Repositories\Abstracts;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query\Expr;
+use Doctrine\ORM\QueryBuilder;
 use LaravelDoctrine\Entities\Abstracts\Entity;
 
 /**
- * @template T of \LaravelDoctrine\Entities\Abstracts\Entity
+ * @template T of Entity
  */
 abstract class Repository extends EntityRepository
 {
-    /**
-     * @return Entity
-     */
-    public function fetchAll(int $page, int $rows): array
-    {
+    public function fetchAll(
+        int $page,
+        int $rows,
+        array $filters = [],
+        array $sorts = [],
+    ): array {
         $start = ($page - 1) * $rows;
 
-        return $this->createQueryBuilder('e')
+        $query = $this->createQueryBuilder('e')
             ->setMaxResults($rows)
-            ->setFirstResult($start)
-            ->getQuery()
-            ->getResult()
-        ;
+            ->setFirstResult($start);
+
+        if (!empty($filters)) {
+            $this->applyFilters($query, $filters);
+        }
+
+        if (!empty($sorts)) {
+            $this->applySorts($query, $sorts);
+        }
+
+        return $query->getQuery()
+            ->getResult();
+    }
+
+    protected function betweenQueryPart(
+        string $operator,
+        Expr $expr,
+        string $qualifiedName,
+        string $parameterName,
+        mixed $value,
+        QueryBuilder $query,
+    ): void {
+        $value = explode($operator, $value);
+
+        $low = "{$parameterName}Low";
+        $high = "{$parameterName}High";
+
+        $part = $expr->between($qualifiedName, $low, $high);
+
+        $query->where($part)
+            ->setParameter($low, $value[0])
+            ->setParameter($high, $value[1]);
+    }
+
+    protected function applyFilters(QueryBuilder $query, array $filters): void
+    {
+        $expr = $query->expr();
+
+        foreach ($filters as $field => $value) {
+            $qualifiedName = "e.{$field}";
+            $parameterName = ":{$field}Values";
+
+            // By default, we just want to use an equals check.
+            $part = $expr->eq($qualifiedName, $parameterName);
+
+            // If $value is a string with commas, explode the string to an array AND run a SQL `e.field IN ()`.
+            if (str_contains($value, ',')) {
+                $value = explode(',', $value);
+                $part = $expr->in($qualifiedName, $parameterName);
+
+                $query->where($part)
+                    ->setParameter($parameterName, $value);
+
+                continue;
+            }
+
+            if (str_contains($value, '>')) {
+                $this->betweenQueryPart('>', $expr, $qualifiedName, $parameterName, $value, $query);
+
+                continue;
+            }
+
+            if (str_contains($value, '<')) {
+                $this->betweenQueryPart('<', $expr, $qualifiedName, $parameterName, $value, $query);
+
+                continue;
+            }
+
+            $query->where($part)
+                ->setParameter($parameterName, $value);
+        }
+    }
+
+    protected function applySorts(QueryBuilder $query, array $sorts): void
+    {
+        $tableAlias = $query->getRootAliases()[0];
+
+        foreach ($sorts as $field) {
+            $tempField = ltrim($field, '-');
+
+            if ($tempField !== $field) {
+                $query->addOrderBy("{$tableAlias}.{$tempField}", 'DESC');
+                continue;
+            }
+
+            $query->addOrderBy("{$tableAlias}.$field", 'ASC');
+        }
     }
 
     public function getCount(): int
@@ -32,8 +118,7 @@ abstract class Repository extends EntityRepository
         return $this->createQueryBuilder('e')
             ->select('COUNT(e)')
             ->getQuery()
-            ->getSingleScalarResult()
-        ;
+            ->getSingleScalarResult();
     }
 
     /**
@@ -58,7 +143,7 @@ abstract class Repository extends EntityRepository
      * @param Entity $entity
      */
     public function delete(
-        \LaravelDoctrine\Entities\Abstracts\Entity $entity,
+        Entity $entity,
         bool $flush = true,
     ): void {
         $entityManager = $this->getEntityManager();
