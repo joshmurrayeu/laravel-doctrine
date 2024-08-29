36f084d815d2a9ed512f06554191830bf38d6fcb
Josh Murray <josh.murray@nmible.com>
2024-08-29 10:36:09 +0100

    Added a refresh command

---
+++ src/Console/Commands/ORM/RefreshDatabaseCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace LaravelDoctrine\Console\Commands\ORM;
+
+use Illuminate\Console\Command;
+use LaravelDoctrine\Console\Commands\Migrations\MigrateCommand;
+use LaravelDoctrine\DataFixtures\Console\Commands\LoadCommand;
+use Symfony\Component\Console\Command\Command as BaseCommand;
+
+class RefreshDatabaseCommand extends Command
+{
+    protected $signature = 'doctrine:orm:refresh';
+
+    public function handle(): int
+    {
+        $commands = $this->getCommands();
+        $progressBar = $this->output->createProgressBar(count($commands));
+
+        $progressBar->start();
+        $progressBar->setFormat("%current%/%max% [%bar%] %percent:3s%% %message%");
+
+        foreach ($commands as $message => $command) {
+            $progressBar->setMessage(' ➡   ' . $message);
+
+            $command();
+
+            $progressBar->advance();
+        }
+
+        $progressBar->setMessage('  <info>Database has been refreshed.</info>');
+        $progressBar->finish();
+
+        $this->newLine();
+
+        return BaseCommand::SUCCESS;
+    }
+
+    protected function getCommands(): array
+    {
+        $args = ['--no-interaction' => true];
+
+        return [
+            'Dropping the database...' => fn() => $this->callSilent(
+                DropCommand::class,
+                $args + [
+                    '--force' => true,
+                    '--full-database' => true
+                ]
+            ),
+            'Migrating the database...' => fn() => $this->callSilent(MigrateCommand::class, $args),
+            'Loading the fixtures...' => fn() => $this->callSilent(LoadCommand::class, $args),
+        ];
+    }
+}
